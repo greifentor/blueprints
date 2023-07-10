59b87903bdec75affe187e0ed89d7bd6dc1e6aab
Oliver.Lieshoff <Oliver.Lieshoff@host.docker.internal>
2023-07-10 16:07:38 +0200

    Corrections.

--- spring-plain/src/main/java/de/ollie/springplain/Application.java
+++ spring-plain/src/main/java/de/ollie/springplain/Application.java
@@ -1,6 +1,5 @@
 package de.ollie.springplain;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
